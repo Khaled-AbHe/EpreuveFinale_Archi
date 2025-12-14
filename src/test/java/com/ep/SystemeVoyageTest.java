0a7c293ee150f09a053b4355bcb520bec7496a46
Khaled Abou Hechmeh <6320936@cegepmv.ca>
2025-12-14 17:50:47 -0500

    Cleaned up test file

--- src/test/java/com/ep/SystemeVoyageTest.java
+++ src/test/java/com/ep/SystemeVoyageTest.java
@@ -1,10 +1,10 @@
-package com.example.demoef;
+package com.ep;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoEfApplicationTests {
+class SystemeVoyageTest {
 
     @Test
     void contextLoads() {
